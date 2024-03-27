27c56679d147254d645bd7003f62354eac0b534f
ZsirosB <zsiros.bence@nrgworks.hu>
2024-03-27 10:33:03 +0100

    revert

--- scripts/git_update.sh
+++ scripts/git_update.sh
@@ -60,7 +60,6 @@ else
   echo "Already a tag on this commit"
 fi
 
-# echo ::set-output name=git-tag::$NEW_TAG
-run echo "git-tag=$NEW_TAG" >>$GITHUB_OUTPUT
+echo ::set-output name=git-tag::$NEW_TAG
 
 exit 0
